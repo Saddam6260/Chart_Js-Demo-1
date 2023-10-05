5a2f4cf01981295889f7b03e163c6e0a59c5727d
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-10-05 23:49:24 +0600

    added another chartber in chartjs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const ctx = document.getElementById("firstChart");
+const firstChart = document.getElementById("firstChart");
 
-new Chart(ctx, {
+new Chart(firstChart, {
   type: "bar",
   data: {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -19,4 +19,28 @@ new Chart(ctx, {
       },
     },
   },
+});
+
+const secountChart = document.getElementById("secoundChart");
+
+const country = ["Bangladesh", "Pakistan", "Egypt", "India", "Insonesia"];
+const bgColor = ["#2a9d8f", "#780000", "#023e8a", "#ff8fab", "#5a189a"];
+const value = [250000, 120000, 108000, 300000, 800000];
+
+new Chart(secountChart, {
+  type: "bar",
+  data: {
+    labels: country,
+    datasets: [{
+      backgroundColor: bgColor,
+      data: value
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Top 5 Country",
+    }
+  }
 });
