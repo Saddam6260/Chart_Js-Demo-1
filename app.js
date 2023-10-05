c2f6f6bf0ea53db266cccd5bf51ed9dc8bb1f034
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-10-05 23:18:01 +0600

    added one chart view

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-import Chart from "chart.js";
+const ctx = document.getElementById("firstChart");
 
-const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yValues = [55, 49, 44, 24, 15];
-const barColors = ["red", "green", "blue", "orange", "brown"];
-
-new Chart("firstChart", {
+new Chart(ctx, {
   type: "bar",
   data: {
-    labels: xValues,
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
       {
-        backgroundColor: barColors,
-        data: yValues,
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
       },
     ],
   },
   options: {
-    legend: { display: false },
-    title: {
-      display: true,
-      text: "World Wine Production 2018",
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
     },
   },
 });
